32474b6954a0b94b7f65a8a45f8afd00d2c51333
Alexey Nebolsin <alexey@nebols.in>
2023-11-28 19:24:19 +0400

    Make navigation buttons work

--- app/header.tsx
+++ app/header.tsx
@@ -3,17 +3,25 @@ import Image from "next/image";
 export function Header() {
   return (
     <header>
-      <Image
-        src="/e-alumni-logo.png"
-        alt="E alumni logo"
-        width={32}
-        height={32}
-      />
+      <a href="/" className="logoContainer">
+        <Image
+          src="/e-alumni-logo.png"
+          alt="E alumni logo"
+          width={32}
+          height={32}
+        />
+      </a>
       <div className="desktopNavigation">
-        <a className="active">Фонд</a>
-        <a>Выпускникам</a>
-        <a>Вложиться в будущее человечества</a>
-        <a>Подать заявку на грант</a>
+        <a href="/fund" className="active">
+          Фонд
+        </a>
+        <a href="/about">Выпускникам</a>
+        <a href="/fund/invest">
+          Вложиться в будущее человечества
+        </a>
+        <a href="/fund/grant">
+          Подать заявку на грант
+        </a>
       </div>
     </header>
   );
